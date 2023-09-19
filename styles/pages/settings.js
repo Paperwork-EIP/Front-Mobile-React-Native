d561db4091e990c516550c155d2d48e335fc1837
faure78 <dylan1.faure@epitech.eu>
2023-09-19 18:09:12 +0200

    [FIX] Bug dbl background color in container settingsLights

--- styles/pages/settings.js
+++ styles/pages/settings.js
@@ -4,7 +4,6 @@ const settingsLight = StyleSheet.create({
     container: {
         width: '100%',
         height: '100%',
-        backgroundColor: '#fff',
         alignItems: 'center',
         paddingVertical: "20%",
         color: "black",
